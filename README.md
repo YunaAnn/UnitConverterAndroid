bca9e22ec1a32d00da06c3ad211d86a0897ec8f4
YunaAnn <ann.grzywnowicz@gmail.com>
2019-11-24 20:25:22 +0100

    temperature conversion

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 TESTED ON: Motorola Moto G6 Plus
 
 TODO:
-- temperature conversion
 - length conversion
 - number conversion
 - possible visual changes
@@ -13,10 +12,14 @@ TODO:
 DONE:
 - Navigation drawer
 - UI
+- temperature conversion
 
-CURRENT APPEARANCE:
+NAVIGATION
 <p align="center">
+  <img src="https://raw.githubusercontent.com/YunaAnn/UnitConverterAndroid/master/Screenshots/navigation.png" width="400" title="hover text">
+
+TEMPERATURE CONVERSION
+  <p align="center">
   <img src="https://raw.githubusercontent.com/YunaAnn/UnitConverterAndroid/master/Screenshots/temperature.png" width="400" title="hover text">
-  <img src="https://raw.githubusercontent.com/YunaAnn/UnitConverterAndroid/master/Screenshots/length.png" width="400" title="hover text">	
-  <img src="https://raw.githubusercontent.com/YunaAnn/UnitConverterAndroid/master/Screenshots/number.png" width="400" title="hover text">	
-  <img src="https://raw.githubusercontent.com/YunaAnn/UnitConverterAndroid/master/Screenshots/navigation.png" width="400" title="hover text">	
+  <img src="https://raw.githubusercontent.com/YunaAnn/UnitConverterAndroid/master/Screenshots/temperature_conversion.png" width="400" title="hover text">
+  <img src="https://raw.githubusercontent.com/YunaAnn/UnitConverterAndroid/master/Screenshots/temperature_error.png" width="400" title="hover text">
--- app/src/main/java/Un4N/unitconverterandroid/MainActivity.java
+++ app/src/main/java/Un4N/unitconverterandroid/MainActivity.java
@@ -2,9 +2,7 @@ package Un4N.unitconverterandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,10 +11,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener
 {
+
+    private double fahrenheit;
+    private double celsius;
+    private double kelvin;
+
+    private TextInputEditText fahrenheitBox;
+    private TextInputEditText celsiusBox;
+    private TextInputEditText kelvinBox;
+    private Button fahrenheitButton;
+    private Button celsiusButton;
+    private Button kelvinButton;
+    private Button resetButton;
+    private TextView status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -34,6 +48,127 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        fahrenheitBox = (TextInputEditText) findViewById(R.id.fahrenheit_box);
+        celsiusBox = (TextInputEditText) findViewById(R.id.celsius_box);
+        kelvinBox = (TextInputEditText) findViewById(R.id.kelvin_box);
+        fahrenheitButton = (Button) findViewById(R.id.fahrenheit_button);
+        celsiusButton = (Button) findViewById(R.id.celsius_button);
+        kelvinButton = (Button) findViewById(R.id.kelvin_button);
+        resetButton = (Button) findViewById(R.id.reset);
+        status = (TextView) findViewById(R.id.status);
+
+        fahrenheitButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                fahrenheitConvertion();
+            }
+        });
+
+        celsiusButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                celsiusConvertion();
+            }
+        });
+
+        kelvinButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                kelvinConvertion();
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                reset();
+            }
+        });
+    }
+
+    public void reset()
+    {
+        fahrenheitBox.setText(" ");
+        celsiusBox.setText(" ");
+        kelvinBox.setText(" ");
+        status.setText("Status : OK");
+    }
+
+    public void fahrenheitConvertion()
+    {
+        try
+        {
+            fahrenheit = Double.parseDouble(fahrenheitBox.getText().toString());
+            celsius = (fahrenheit - 32) / 1.8;
+            kelvin = (fahrenheit + 459.67) / 1.8;
+            celsiusBox.setText(String.valueOf(celsius));
+            kelvinBox.setText(String.valueOf(kelvin));
+            status.setText("Status : OK");
+        }
+        catch(NumberFormatException e)
+        {
+            status.setText("Status : No data inserted. Try again. Error -> " + e.getMessage());
+            e.printStackTrace();
+        }
+        catch(Exception e)
+        {
+            status.setText("Status : Try again. Error ->" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void celsiusConvertion()
+    {
+        try {
+            celsius = Double.parseDouble(celsiusBox.getText().toString());
+            fahrenheit = celsius * 1.8 + 32;
+            kelvin = celsius + 273.15;
+            fahrenheitBox.setText(String.valueOf(fahrenheit));
+            kelvinBox.setText(String.valueOf(kelvin));
+            status.setText("Status : OK");
+        }
+         catch(NumberFormatException e)
+        {
+            status.setText("Status : No data inserted. Try again. Error -> " + e.getMessage());
+            e.printStackTrace();
+        }
+        catch(Exception e)
+        {
+            status.setText("Status : Try again. Error ->" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void kelvinConvertion()
+    {
+        try
+        {
+            kelvin = Double.parseDouble(kelvinBox.getText().toString());
+            fahrenheit = kelvin * 1.8 - 459.67;
+            celsius = kelvin - 273.15;
+            fahrenheitBox.setText(String.valueOf(fahrenheit));
+            celsiusBox.setText(String.valueOf(celsius));
+            status.setText("Status : OK");
+        }
+         catch(NumberFormatException e)
+        {
+            status.setText("Status : No data inserted. Try again. Error -> " + e.getMessage());
+            e.printStackTrace();
+        }
+        catch(Exception e)
+        {
+            status.setText("Status : Try again. Error ->" + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     @Override
